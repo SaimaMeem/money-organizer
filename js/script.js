48c06e710d9bcb1fa7b5e037ef5a2798b63d2b03
SaimaMeem <saimasultana1320@gmail.com>
2022-02-18 23:14:47 +0600

    mandatory error handling done

--- js/script.js
+++ js/script.js
@@ -1,14 +1,16 @@
+const incomeInput = document.getElementById("income");
+const foodInput = document.getElementById("food");
+const rentInput = document.getElementById("rent");
+const clothInput = document.getElementById("cloth");
+const totalExpenses = document.getElementById("total-expenses");
+const balanceAmount = document.getElementById("balance");
+const saveInput = document.getElementById("save");
+const savingAmount = document.getElementById("saving-amount");
 const saveButton = document.getElementById("save-button");
 const calculateButton = document.getElementById("calculate-button");
 
-
 function calculate() {
-    const incomeInput = document.getElementById("income");
-    const foodInput = document.getElementById("food");
-    const rentInput = document.getElementById("rent");
-    const clothInput = document.getElementById("cloth");
-    const totalExpenses = document.getElementById("total-expenses");
-    const balanceAmount = document.getElementById("balance");
+
     // expense calculation
     const totalEx = parseFloat(foodInput.value) + parseFloat(rentInput.value) + parseFloat(clothInput.value);
     totalExpenses.innerText = totalEx.toLocaleString();
@@ -17,7 +19,6 @@ function calculate() {
     //balance calculation
     const totalBAmount = parseFloat(incomeInput.value) - totalEx;
     balanceAmount.innerText = totalBAmount.toLocaleString();
-    // balanceAmount.innerText = totalBAmount;
     //balance calculation
     foodInput.value = '';
     rentInput.value = '';
@@ -27,8 +28,6 @@ function calculate() {
 }
 
 function save() {
-    const saveInput = document.getElementById("save");
-    const savingAmount = document.getElementById("saving-amount");
     const remainingBalance = document.getElementById("rem-balance");
     const balanceAmount = document.getElementById("balance");
     //removing thousands seperator and converting into a number
@@ -38,7 +37,41 @@ function save() {
     const remaining = tempBalance - saved;
     remainingBalance.innerText = remaining.toLocaleString();
     saveInput.value = '';
-    // console.log(tempBalance);
+}
+
+function validateInputFields(val, inputName) {
+    const value = parseFloat(val.value);
+    const errorMessage = document.getElementById(inputName + '-error-message');
+    console.log(errorMessage);
+    if (isNaN(value) || value < 0) {
+        // console.log(inputName + " amount is invalid. Please enter a positive number!");
+        errorMessage.style.visibility = 'visible';
+    }
 }
 calculateButton.addEventListener('click', calculate);
 saveButton.addEventListener('click', save);
+incomeInput.addEventListener('focusout', function() {
+    validateInputFields(this, 'income');
+});
+foodInput.addEventListener('focusout', function() {
+    validateInputFields(this, 'food');
+});
+rentInput.addEventListener('focusout', function() {
+    validateInputFields(this, 'rent');
+});
+clothInput.addEventListener('focusout', function() {
+    validateInputFields(this, 'cloth');
+});
+
+incomeInput.addEventListener('focus', function() {
+    document.getElementById(this.id + '-error-message').style.visibility = 'hidden';
+});
+foodInput.addEventListener('focus', function() {
+    document.getElementById(this.id + '-error-message').style.visibility = 'hidden';
+});
+rentInput.addEventListener('focus', function() {
+    document.getElementById(this.id + '-error-message').style.visibility = 'hidden';
+});
+clothInput.addEventListener('focus', function() {
+    document.getElementById(this.id + '-error-message').style.visibility = 'hidden';
+});
